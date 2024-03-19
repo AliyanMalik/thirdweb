f0ff0d26dd71f05d03a20f8b50580df0a1256343
Muhammad Taha <Aliyan.malik.am@gmail.com>
2024-03-19 06:02:53 +0500

    Built component

---
+++ docs/build/thirdweb.esm.js
@@ -0,0 +1,2 @@
+import{p as t,b as o}from"./p-1b168fb0.js";export{s as setNonce}from"./p-1b168fb0.js";import{g as e}from"./p-e1255160.js";const r=()=>{const o=import.meta.url;const s={};if(o!==""){s.resourcesUrl=new URL(".",o).href}return t(s)};r().then((async t=>{await e();return o([["p-d4c67ece",[[1,"my-thirdweb-component"]]]],t)}));
+//# sourceMappingURL=thirdweb.esm.js.map
